b8b61839c79617c4aede7a1a1f689cca49355b99
mohamed salah <mohamedsalah.owida@gmail.com>
2025-06-12 18:50:21 +0300

    ULTRASONIC Module

---
+++ HAL/Ultrasonic/ULTRASONIC_Cfg.h
@@ -0,0 +1,15 @@
+/*
+ * ULTRASONIC_Cfg.h
+ *
+ *  Created on: Mar 21, 2025
+ *      Author: moham
+ */
+
+#ifndef ULTRASONIC_CFG_H_
+#define ULTRASONIC_CFG_H_
+
+
+#define TriggerPort  DIO_u8PORTC
+#define TriggerPin   DIO_u8PIN0
+
+#endif /* ULTRASONIC_CFG_H_ */
---
+++ HAL/Ultrasonic/ULTRASONIC_Interface.h
@@ -0,0 +1,21 @@
+/*
+ * ULTRASONIC_Interface.h
+ *
+ *  Created on: Mar 21, 2025
+ *      Author: moham
+ */
+
+#ifndef ULTRASONIC_INTERFACE_H_
+#define ULTRASONIC_INTERFACE_H_
+
+
+
+void Ultrasonic_voidInit(void);
+
+uint16 Ultrasonic_u16GetDistance(void);
+
+
+
+
+
+#endif /* ULTRASONIC_INTERFACE_H_ */
